d0b10d087b9162dd930a1ce8014f3e8528a90226
Raymunfil Anxa <raymunfilanxa@osx-yosi.local>
2014-11-18 14:45:57 +0800

    Initial Commit

--- exer1/exer1/ViewController.swift
+++ exer1/exer1/ViewController.swift
@@ -1,24 +1,18 @@
 //
 //  ViewController.swift
-//  SingleViewApp
+//  exer1
 //
-//  Created by Raymunfil Anxa on 10/23/14.
+//  Created by Raymunfil Anxa on 11/18/14.
 //  Copyright (c) 2014 Anxa. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-    
-@IBOutlet var webView: UIWebView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        // Do any additional setup after loading the view, typically from a nib.
-        var url = NSURL(string: "http://m.hapi.com/account")
-        var request = NSURLRequest(URL: url!)
-        webView.loadRequest(request)
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,10 +20,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-
-    }
 
 }
 
